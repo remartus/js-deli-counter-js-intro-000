4381804c7536c53c4aa2925a6f708bbfd4a5161c
Rose Martus <martus.rose@gmail.com>
2021-03-27 20:50:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function takeANumber(currentLine, newName){
   console.log(currentLine)
   currentLine.concat(newName)
   console.log(currentLine)
+  return `Welcome, ${newName}. You are number ${currentLine.length + 1} in line.`
 }
