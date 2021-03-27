df5d6f9764819cec32bc67c243d460ef3086529e
Rose Martus <martus.rose@gmail.com>
2021-03-27 21:18:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function currentLine(katzDeliLine){
     for(let i = 1; i < katzDeliLine.length; i++){
       response = response + i + ". " + katzDeliLine[i-1] +", "
     }
-
+    response = response + katzDeliLine.length + ". " + katzDeliLine[katzDeliLine.length - 1] + "."
     return response
   }
 }
