a02e60885c631c21f391f7f3d255bcf4e520b3ef
Rose Martus <martus.rose@gmail.com>
2021-03-27 20:52:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //takeANumber adds a person to the line, and handles multiple people being added
 function takeANumber(currentLine, newName){
   console.log(currentLine)
-  currentLine.concat(newName)
+  currentLine.concat(newName);
   console.log(currentLine)
   return `Welcome, ${newName}. You are number ${currentLine.length + 1} in line.`
 }
